d524ad94444f68a33fbc72fd4a0f6e37e9827281
caborryer <corraleshyulia@gmail.com>
2020-02-11 06:46:32 -0500

    rendering text component

--- src/Components/TextComponent/TextComponent.js
+++ src/Components/TextComponent/TextComponent.js
@@ -1,31 +1,21 @@
 import React, {useEffect, useState} from 'react';
 import Papa from 'papaparse'
 
-
-
 const TextComponent = () => {
   const [csv, setCsv] = useState('');
-
 
   const handleChange = e => {
     setCsv({
       ...csv,
       [e.target.name]: e.target.value
     })
-    const change = Papa.parse(csv)
-    console.log(change)
   }
-
-  useEffect(() => {
-    const processData = () => {
-
-    }
-  }, [csv])
-
 
   const handleSubmit = e => {
     e.preventDefault();
     console.log(csv)
+    //const change = Papa.parse(csv,{delimiter:"\t", header:true})
+    //console.log(change)
   }
 
 
@@ -33,14 +23,15 @@ const TextComponent = () => {
     <div className="container">
       <h1>Paste your cvs dates</h1>
       <div className="principalContend contend">
-        <div className="input-group">
-          <div className="input-group-prepend">
+        <form >
+          <div className="input-group">
+            <div className="input-group-prepend">
+            </div>
+            <textarea onChange={handleChange} name="csv" className="form-control" aria-label="With textarea"></textarea>
           </div>
-          <textarea onChange={handleChange} name="csv" className="form-control" aria-label="With textarea"></textarea>
-        </div>
-        <br/>
-
-        <button onSubmit={handleSubmit} type="button" className="btn btn-primary">Process</button>
+          <br/>
+          <button onClick={handleSubmit}  type="button" className="btn btn-primary">Process</button>
+        </form>
       </div>
     </div>
   );
